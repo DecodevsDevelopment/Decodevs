72e094a9255761335a5017ad6ecc6d1589379619
Nicolas <nicoeei@gmail.com>
2024-05-28 14:17:39 -0300

    Modifique navbar mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -74,8 +74,8 @@ export const Navbar = () => {
       </nav>
       {/* Panel de menú desplegable para dispositivos móviles */}
       <Dialog className='lg:hidden' open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className='fixed inset-0 z-50' />
-        <Dialog.Panel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-700 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+        <div className='fixed inset-0 z-50 ' />
+        <Dialog.Panel className='fixed inset-y-0 h-2/4 right-0 z-50 w-full overflow-y-auto bg-gray-700 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
             {/* Logo a la izquierda */}
             <Link to='/' className='-m-1.5 p-1.5'>
@@ -90,10 +90,10 @@ export const Navbar = () => {
           </div>
           <div className='mt-6 flow-root'>
             <div className='-my-6 divide-y divide-gray-500/10'>
-              <div className='space-y-2 py-6'>
+              <div className='space-y-2 py-6 flex text-center flex-col justify-center'>
                 {/* Elementos de navegación */}
                 {navigation.map((item) => (
-                  <Link key={item.name} to={item.path} className='-mx-3 block rounded-lg px-3 py-2 text-base navbar font-semibold leading-7 text-white hover:bg-gray-50'>
+                  <Link key={item.name} to={item.path} className='-mx-3 block rounded-lg px-3 py-2 text-base navbar font-semibold leading-7 text-white'>
                     {item.name}
                   </Link>
                 ))}
